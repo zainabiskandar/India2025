bb3cd8df959c5527b57ed5d5f1ff3f55d6544f0c
zainabiskandar <82629674+zainabiskandar@users.noreply.github.com>
2025-10-05 20:10:43 +0800

    Update vite.config.ts

--- Digital Reflection Journal/vite.config.ts
+++ Digital Reflection Journal/vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/India2025/',   // <-- use your repo name here
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
